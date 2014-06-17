049478b317d38b3b151025a4a04e8c19123c6bc5
Raj Rao <neeraj2608@gmail.com>
2014-06-17 02:36:42 -0400

    Plain vanilla pagerank done.

--- Graph.py
+++ Graph.py
@@ -1,80 +1,212 @@
 # Graph
 # Implements Weighted-PageRank
 
+import numpy as np
+
 DAMPING = 0.85
+MAX_ITER = 1000
+TOL = 1e-6
+PAGERANKMETHODS = ['powermethod','iterativemethod']
 
 class Graph:
     def __init__(self):
         self.vertSet = {}
-        self.size = 0
+        self.vertNum = {}
+        self.N = 0
 
     def addVertex(self, key):
         if not key in self.vertSet:
-            self.size += 1
             self.vertSet[key] = Vertex(key)
+            self.vertNum[key] = self.N
+            self.N += 1
+
+    def addEdge(self, fromKey, toKey, edgeWeight=0):
+        if not fromKey in self:
+            self.addVertex(fromKey)
+        self.getVertex(fromKey).outSet[toKey] = edgeWeight # Adds outcoming connection from vertex
+        if not toKey in self:
+            self.addVertex(toKey)
+        self.getVertex(toKey).inSet[fromKey] = edgeWeight # Adds incoming connection to vertex
 
     def getVertex(self, key):
         return self.vertSet.get(key)
 
+    def getVertices(self):
+        return self.vertSet.values()
+
+    def getPlainVanillaPageRank(self, method=PAGERANKMETHODS[0]):
+        '''
+        Plain-vanilla Pagerank.
+        '''
+        if method not in PAGERANKMETHODS:
+            raise PageRankNotAvailableException("'method' parameter must be one of the following: %s" % PAGERANKMETHODS)
+
+        if self.N == 0:
+            raise PageRankNotAvailableException("empty graph!")
+
+        pRank = np.ones(self.N) / self.N # initially all 1/N
+        if(method == PAGERANKMETHODS[0]):
+            M = self.buildM()
+            power = self.powerMethod(pRank, M)
+            print power
+            return power
+        else:
+            A = self.buildA()
+            it = self.iterative(pRank, A)
+            print it
+            return it
+
+    def powerMethod(self, pRank, M):
+        '''
+        Calculate pagerank using the power method.
+        '''
+        M_hat = (DAMPING*M) + ((1-DAMPING)/self.N)
+
+        for i in xrange(MAX_ITER):
+            newPRank = np.dot(M_hat,pRank)
+            err = np.abs(newPRank-pRank).sum()
+            if err < self.N*TOL:
+                return newPRank/np.linalg.norm(newPRank)
+            pRank = newPRank
+        raise PageRankNotAvailableException('Pagerank did not terminate within %d iterations' % MAX_ITER)
+
+    def buildM(self):
+        '''
+        Builds the Google Matrix or M.
+        This matrix needs to be calculated only once per invocation of the Pagerank algorithm.
+        '''
+
+        A = self.buildA()
+
+        # replace zero columns with initial probability 1/N
+        sumA = np.sum(A, axis=0)
+        nonzeroindices = np.nonzero(sumA)
+        sumA[sumA==0] += float(1)/self.N
+        sumA[nonzeroindices] = 0
+        M = A + np.tile(sumA,(self.N,1))
+
+        return M
+
+    def buildA(self):
+        '''
+        Returns an out-degree matrix that has elements set to 1/outdegree(x) for all
+        elements (x,y) where vertex x connects to vertex y.
+
+        E.g. for a graph with 4 vertices 0, 1, 2, 3 with
+        0 -> 1
+        0 -> 2
+        3 -> 2,
+        the following matrix is returned:
+        [[ 0.   0.   0.   0. ]
+         [ 0.5  0.   0.   0. ]
+         [ 0.5  0.   0.   1. ]
+         [ 0.   0.   0.   0. ]]
+
+        Explanation:
+        element (1,0) is non-zero because node 0 is connected to node 1. It is 0.5 because node 0 has outdegree TWO (two outgoing edges)
+        element (2,0) is non-zero because node 0 is connected to node 2. It is 0.5 because node 0 has outdegree TWO (two outgoing edges)
+        element (2,3) is non-zero because node 3 is connected to node 2. It is 1   because node 1 has outdegree ONE (one outgoing edges)
+
+        WARNING! Note that the direction is from column to row!!! (1,0) being non-zero indicates a connection FROM 0 TO 1 and NOT from 1 to 0!!
+
+        Note: This matrix needs to be calculated only per invocation of the Pagerank algorithm.
+        '''
+        A = np.zeros((self.N, self.N))
+        _A = [elem for vertex in self for elem in \
+                            map(lambda x: [[self.vertNum[vertex.id], self.vertNum[x]], \
+                                            self.getVertex(x).getOutDegree()], \
+                                vertex.getIncomingKeys())]
+        _A = np.dstack(_A)[0]
+        connectedVertices = np.dstack(_A[0])[0]
+        outDegrees = _A[1]
+        A[list(connectedVertices)] = float(1) / outDegrees
+        return A
+
+    def iterative(self, pRank, A):
+        '''
+        Calculate pagerank using the iterative method.
+        '''
+        newPRank = np.dot(DAMPING*A,pRank) + ((1-DAMPING)/self.N)
+        err = np.abs(newPRank-pRank).sum()
+        if err < TOL:
+            return newPRank
+        it = self.iterative(newPRank, A)
+        return it/np.linalg.norm(it)
+
     def __getitem__(self, key):
-        return getVertex(key)
+        return self.getVertex(key)
 
     def __contains__(self, key):
         return key in self.vertSet
 
-    def getVertices(self):
-        return self.vertSet.values()
-
     def __iter__(self):
-        return iter(getVertices())
-
-    def addEdge(self, fromKey, toKey, edgeWeight=0):
-        if not fromKey in self:
-            addVertex(fromKey)
-        self.getVertex(fromKey).addNeighbor(toKey, edgeWeight)
-
-    def getPageRank(self):
-        '''
-        Plain-vanilla Pagerank.
-        '''
-        pass # TODO
+        return iter(self.getVertices())
 
 class Vertex:
     def __init__(self, key):
         self.id = key
-        self.adjset = {}
+        self.outSet = {} # outgoing edges
+        self.inSet = {} # incoming edges
 
-    def addNeighbor(self, key, weight=0):
+    def getIncomingKeys(self):
         '''
-        Adds connection to vertex with key = key
+        Returns a list of keys of vertices connected to
+        this node by incoming edges.
         '''
-        self.adjset[key] = weight
+        return self.inSet.keys()
 
-    def getConnectedKeys(self):
-        '''
-        Returns a list of keys of connected vertices.
-        '''
-        return self.adjset.keys()
+    def getInDegree(self):
+        return len(self.inSet.keys())
 
-    def getWeight(self, v):
-        return self.adjset[v.key]
+    def getOutDegree(self):
+        return len(self.outSet.keys())
+
+    def getOutgoingKeys(self):
+        '''
+        Returns a list of keys of vertices connected to
+        this node by outgoing edges.
+        '''
+        return self.outSet.keys()
+
+class PageRankNotAvailableException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 def testGraph():
     g = Graph()
     g.addVertex('a')
-    g.getVertex('a').addNeighbor('a1', 10)
+    g.addEdge('a','d',10)
     g.addVertex('b')
     g.addVertex('c')
-    assert g.size == 3
+    assert g.N == 4
     assert g.getVertex('a') <> None
-    assert g.getVertex('d') == None # non-existent key
+    assert g.getVertex('e') == None # non-existent key
+    assert 'd' in g.getVertex('a').getOutgoingKeys()
+    assert 'a' in g.getVertex('d').getIncomingKeys()
 
     g.addVertex('a')                                   # key 'a' is already in the graph
-    assert g.size == 3                                 # nothing should be added
-    assert 'a1' in g.getVertex('a').getConnectedKeys() # the old node should be left untouched
+    assert g.N == 4                                 # nothing should have been added
+    assert 'd' in g.getVertex('a').getOutgoingKeys() # the old node should have been left untouched
 
     g.addEdge('a','b')
-    assert 'b' in g.getVertex('a').getConnectedKeys()
+    g.addEdge('c','b')
+    assert 'b' in g.getVertex('a').getOutgoingKeys()
+    assert 'c' in g.getVertex('b').getIncomingKeys()
+
+    g.getPlainVanillaPageRank()
+
+    g1 = Graph()
+    g1.addVertex('a')
+    g1.addVertex('b')
+    g1.addVertex('c')
+    g1.addEdge('a','b')
+    assert 'b' in g1.getVertex('a').getOutgoingKeys()
+    assert 'a' in g1.getVertex('b').getIncomingKeys()
+
+    g1.getPlainVanillaPageRank(method='iterativemethod')
 
 if __name__ == '__main__':
     testGraph()
