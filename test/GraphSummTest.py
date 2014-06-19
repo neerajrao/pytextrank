2a1780593f049a1baa3f1479d53ad393268327b1
Raj Rao <neeraj2608@gmail.com>
2014-06-19 12:50:15 -0400

    Minor cleanup.

--- test/GraphSummTest.py
+++ test/GraphSummTest.py
@@ -10,18 +10,21 @@ class GraphSummTest(TestCase):
     def setUp(self):
         self.file_name = path.join(CORPUSPATH,'test2.txt')
         self.test_text = open(self.file_name,'r').read()
-        self.expected_output_pagerank = [u"A major economic great power , Japan has the world 's third-largest economy by nominal GDP and the world 's fourth-largest economy by purchasing power parity .",
-                           u"Although Japan has officially renounced its right to declare war , it maintains a modern military with the world 's eighth largest military budget , used for self-defense and peacekeeping roles .",
-                           u"Japan has the world 's tenth-largest population , with over 126 million people .",
-                           u"The four largest islands are Honshu , Hokkaido , Kyushu , and Shikoku , which together comprise about ninety-seven percent of Japan 's land area ."]
-        self.expected_output_hits_auth = [u"A major economic great power , Japan has the world 's third-largest economy by nominal GDP and the world 's fourth-largest economy by purchasing power parity .",
- u"Japan has the world 's tenth-largest population , with over 126 million people .",
- u"Although Japan has officially renounced its right to declare war , it maintains a modern military with the world 's eighth largest military budget , used for self-defense and peacekeeping roles .",
- u"The four largest islands are Honshu , Hokkaido , Kyushu , and Shikoku , which together comprise about ninety-seven percent of Japan 's land area ."]
-        self.expected_output_hits_hubs = [u'Japan entered into a long period of isolation in the early 17th century , which was only ended in 1853 when a United States fleet pressured Japan to open to the West .',
- u'However , Japan is also substantially prone to earthquakes and tsunami , having the highest natural disaster risk in the developed world .',
- u'Located in the Pacific Ocean , it lies to the east of the Sea of Japan , China , North Korea , South Korea and Russia , stretching from the Sea of Okhotsk in the north to the East China Sea and Taiwan in the south .',
- u"Honsh\u016b 's Greater Tokyo Area , which includes the de facto capital of Tokyo and several surrounding prefectures , is the largest metropolitan area in the world , with over 30 million residents ."]
+        self.expected_output_pagerank = \
+                        [u"A major economic great power , Japan has the world 's third-largest economy by nominal GDP and the world 's fourth-largest economy by purchasing power parity .",
+                          u"Although Japan has officially renounced its right to declare war , it maintains a modern military with the world 's eighth largest military budget , used for self-defense and peacekeeping roles .",
+                          u"Japan has the world 's tenth-largest population , with over 126 million people .",
+                          u"The four largest islands are Honshu , Hokkaido , Kyushu , and Shikoku , which together comprise about ninety-seven percent of Japan 's land area ."]
+        self.expected_output_hits_auth = \
+                        [u"A major economic great power , Japan has the world 's third-largest economy by nominal GDP and the world 's fourth-largest economy by purchasing power parity .",
+                          u"Japan has the world 's tenth-largest population , with over 126 million people .",
+                          u"Although Japan has officially renounced its right to declare war , it maintains a modern military with the world 's eighth largest military budget , used for self-defense and peacekeeping roles .",
+                          u"The four largest islands are Honshu , Hokkaido , Kyushu , and Shikoku , which together comprise about ninety-seven percent of Japan 's land area ."]
+        self.expected_output_hits_hubs = \
+                        [u'Japan entered into a long period of isolation in the early 17th century , which was only ended in 1853 when a United States fleet pressured Japan to open to the West .',
+                          u'However , Japan is also substantially prone to earthquakes and tsunami , having the highest natural disaster risk in the developed world .',
+                          u'Located in the Pacific Ocean , it lies to the east of the Sea of Japan , China , North Korea , South Korea and Russia , stretching from the Sea of Okhotsk in the north to the East China Sea and Taiwan in the south .',
+                          u"Honsh\u016b 's Greater Tokyo Area , which includes the de facto capital of Tokyo and several surrounding prefectures , is the largest metropolitan area in the world , with over 30 million residents ."]
 
     def test_summarize_file_pagerank(self):
         self.assertEqual(graph_summ.summarize_file(self.file_name), self.expected_output_pagerank)
