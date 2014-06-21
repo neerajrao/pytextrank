ef5680f18944e2ec3353bec23bba0c304e9072ff
Raj Rao <neeraj2608@gmail.com>
2014-06-21 16:14:32 -0400

    Fixing name that was changed by mistake.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='Extractive summarization using Graph-based Ranking Models (TextRank)',
+setup(name='graph_summ',
       version='0.1',
       description='Text Summarization using Graph-based Ranking Models',
       url='https://github.com/neeraj2608/graph-ranked-summarization',
